8b4f6ac816a1bec029f867f0e116d24af2494170
asheikh184 <asheikh184@gmail.com>
2022-08-07 01:21:45 +0500

    t

--- src/components/leaderboard/LeaderBoard.jsx
+++ src/components/leaderboard/LeaderBoard.jsx
@@ -1,16 +1,18 @@
-import { Button, Heading, Image, Menu, MenuButton, Stack } from '@chakra-ui/react'
+import { Button, Heading, Image, Img, Menu, MenuButton, Stack, Text } from '@chakra-ui/react'
 import React from 'react'
 import logo from '../../assets/images/logo.png'
+import down from '../../assets/images/down.svg'
 import "../../App.css";
-import { BsFillCaretDownFill } from 'react-icons/bs'
+import LeaderboardTable from './LeaderboardTable';
 
 
 const LeaderBoard = () => {
     return (
         <>
-            <Stack bg={"#050826"} h={"100vh"} px={'12'} py={'10'}>
+            <Stack bg={"#050826"} minH={"100vh"} px={{ base: '2', sm: '12' }} py={'10'} spacing={'6'}>
+                {/* Leaderboard heading Logo and Button */}
                 <Stack direction={'row'} align={'end'}>
-                    <Stack >
+                    <Stack display={{ base: 'none', md: 'inherit' }}>
                         <Button w={'36'} _hover={{ textDecoration: 'none' }} className="button-border" variant={"link"}>
                             <Heading fontSize={"lg"} color={'white'} fontWeight="700">
                                 Home
@@ -32,32 +34,46 @@ const LeaderBoard = () => {
                             </Heading>
                         </Button>
                     </Stack>
-                    <Stack align={'center'} flex={'1'}>
-                        <Heading as={'h1'} fontSize={'5xl'} bgGradient='linear(180deg, #FCFF00 40%, #3FFF13 100%)' bgClip='text'>Leaderboard</Heading>
+                    <Stack align={'center'} flex={'1'} direction={{ base: 'row', md: 'column' }} justify={{ base: 'space-between', md: 'inherit' }}>
+                        <Heading as={'h1'} fontSize={{ base: '2xl', sm: '5xl', md: '5xl' }} bgGradient='linear(180deg, #FCFF00 40%, #3FFF13 100%)' bgClip='text'>Leaderboard</Heading>
                         <Image
                             _hover={{ cursor: "pointer" }}
-                            w={{ base: "20", md: "28", lg: "40" }}
+                            w={{ base: "14", sm: '20', md: "28", lg: "40" }}
                             src={logo}
                         />
                     </Stack>
-                    <Stack w={'36'} />
+                    <Stack w={'36'} display={{ base: 'none', md: 'inherit' }} />
                 </Stack>
-                <Stack bgGradient={'linear-gradient(180deg, #FCFF00 0%, #3FFF13 100%)'} >
+                {/* League Friends Tournament bar */}
+                <Stack bgGradient={'linear-gradient(180deg, #FCFF00 0%, #3FFF13 100%)'} direction={'row'} justify={'space-between'} px={{ md: '16' }} borderRadius={'lg'}>
                     <Menu>
-                        <MenuButton as={Button} rightIcon={<BsFillCaretDownFill />}>
-                            Actions
+                        <MenuButton as={Button} _hover={{}} _focus={{}} _active={{}} variant={'ghost'}>
+                            <Stack direction={'row'} align={'center'} spacing={'1'}>
+                                <Text fontSize={{ base: '0.8em', md: 'lg' }} fontWeight={'bold'}>League</Text>
+                                <Img src={down} w={{ base: '3', sm: '4' }} />
+                            </Stack>
                         </MenuButton>
                     </Menu>
                     <Menu>
-                        <MenuButton as={Button} rightIcon={<BsFillCaretDownFill />}>
-                            Actions
+                        <MenuButton as={Button} _hover={{}} _focus={{}} _active={{}} variant={'ghost'}>
+                            <Stack direction={'row'} align={'center'} spacing={'1'}>
+                                <Text fontSize={{ base: '0.8em', md: 'lg' }} fontWeight={'bold'}>Friends</Text>
+                                <Img src={down} w={{ base: '3', sm: '4' }} />
+                            </Stack>
                         </MenuButton>
                     </Menu>
                     <Menu>
-                        <MenuButton as={Button} rightIcon={<BsFillCaretDownFill />}>
-                            Actions
+                        <MenuButton as={Button} _hover={{}} _focus={{}} _active={{}} variant={'ghost'}>
+                            <Stack direction={'row'} align={'center'} spacing={'1'}>
+                                <Text fontSize={{ base: '0.8em', md: 'lg' }} fontWeight={'bold'}>Tournament</Text>
+                                <Img src={down} w={{ base: '3', sm: '4' }} />
+                            </Stack>
                         </MenuButton>
                     </Menu>
+                </Stack>
+                {/* Table Container */}
+                <Stack border={'1px solid white'} borderRadius={'lg'} p={'4'} bgColor={'rgba(255, 255, 255, 0.2)'} >
+                    <LeaderboardTable />
                 </Stack>
             </Stack>
         </>
